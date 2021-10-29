426e48cc93c0d35907dcade77316e20f1b755125
SaudKadiri <saudkadiri@gmail.com>
2021-10-29 19:53:53 +0530

    Alert shows the sentiment type of the page

--- scripts/analyze.js
+++ scripts/analyze.js
@@ -1,7 +1,12 @@
 /**
- * Called from popop.js this page_analysis() gets called.
+ * Called from popup.js this page_analysis() gets called.
  */
+
 (function page_analysis() {
+    window.sentiment = {
+        "Positive": 0,
+        "Negative": 0
+    }
     let paragraphs = document.getElementsByTagName("p");
     highlight_text(paragraphs);
     let list = document.getElementsByTagName("li");
@@ -10,7 +15,19 @@
     highlight_text(span);
     let h1 = document.getElementsByTagName("h1");
     highlight_text(h1);
+
+
+    alert("Page analysis done!\nSentiment type: " + sentiment_type());
 })();
+
+/**
+ * Function to look out what is the sentiment type of the page!
+ * @returns sentiment type of the page.
+ */
+function sentiment_type() {
+    var max = Math.max.apply(null, Object.keys(sentiment).map(function (x) { return sentiment[x] }));
+    return Object.keys(sentiment).filter((x) => { return sentiment[x] == max; })[0];
+}
 
 /**
  * Highlights the text within the passed tag.
@@ -34,6 +51,10 @@ function get_color(text) {
         "Positive": "#B4E001",  // Green
         "Negative": "#CE3536"   // Red
     };
-    let type = quick_sentiment(text)
+    let type = quick_sentiment(text);
+    // Don't care about the neutral value!
+    if (type != "Neutral") {
+        sentiment[type]++;
+    }
     return colors[type];
 }
